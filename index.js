e945d1a938e5231d7dd4c2ca7ec4e8893e7adab5
Davi André Gauze <115033228+DaviGauze@users.noreply.github.com>
2025-04-01 21:10:37 -0300

    Desenvolvida a conexão com o banco de dados

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 import express from "express";
+import { Sequelize } from "sequelize";
+
+//Conexão com o banco de dados
+const sequelize = new Sequelize('progdb', 'postgres', '1205', {
+    host: 'localhost',
+    port: '5432',
+    dialect: 'postgres',
+    define: {
+        timestamps: false,
+        freezeTableName: true
+    }
+});
+
+//Testando Conexão
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+
 
 const app = express();
 app.use(express.json());
@@ -10,3 +31,4 @@ app.get('/teste', (req, res)=>{
 app.listen(3000, ()=>{
     console.log(`Servidor Rodando`)
 });
+
