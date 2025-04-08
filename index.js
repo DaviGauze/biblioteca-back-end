e7859b756a4fcdd3dc0d29d5be19df81d3b01340
Davi André Gauze <115033228+DaviGauze@users.noreply.github.com>
2025-04-08 19:51:50 -0300

    Desenvolvido os metodos GET, PUT, POST e DELETE

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Bibliotecas
 import express from "express";
 import { Sequelize, DataTypes } from "sequelize";
 
@@ -53,7 +54,62 @@ app.get('/teste', (req, res)=>{
     res.send('teste ok.')
 })
 
+//Rotas Crud da tabela editoras
+app.get('/editora', async (req, res) => {
+    const respostaBanco = await Editora.findAll();
+    res.json(respostaBanco);
+});
+
+app.get('/editora/:id', async (req, res) => {
+    const id = req.params.id;
+    const respostaBanco = await Editora.findByPk(id);
+    res.json(respostaBanco);
+});
+
+app.post('/editora', async (req, res) => {
+//    const nomeeditora = req.body.nomeeditora;
+//    const cnpj = req.body.cnpj;
+//    const endereco = req.body.endereco;
+
+    const respostaBanco = await Editora.create(req.body);
+    res.json(respostaBanco);
+});
+
+app.put('/editora/:id', async (req, res) => {
+    const nomeeditora = req.body.nomeeditora;
+    const cnpj = req.body.cnpj;
+    const endereco = req.body.endereco;
+
+    const ideditora = req.params.id;
+    
+        const respostaBanco = await Editora.update(
+            {nomeeditora, cnpj, endereco},
+            {where: {ideditora}});
+        res.json(respostaBanco);
+    });
+
+app.put('/editora/:id', async (req, res) => {
+    const nomeeditora = req.body.nomeeditora;
+    const cnpj = req.body.cnpj;
+    const endereco = req.body.endereco;
+
+    const ideditora = req.params.id;
+    
+        const respostaBanco = await Editora.update(
+            {nomeeditora, cnpj, endereco},
+            {where: {ideditora}});
+        res.json(respostaBanco);
+    });
+
+app.delete('/editora/:id', async (req, res) => {
+    const ideditora = req.params.id;
+
+    const respostaBanco = await Editora.destroy(
+    {where: {ideditora}});
+    res.json(respostaBanco);
+});    
+
+
 app.listen(3000, ()=>{
     console.log(`Servidor Rodando`)
 });
-
