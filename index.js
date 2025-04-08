d96f5a9b6220803181e3be1f552c47573d3d44d0
Davi André Gauze <115033228+DaviGauze@users.noreply.github.com>
2025-04-08 20:35:29 -0300

    Removida conexão com o banco e inseridas novas funcoes do controller

--- index.js
+++ index.js
@@ -1,51 +1,7 @@
 //Bibliotecas
 import express from "express";
-import { Sequelize, DataTypes } from "sequelize";
-
-//Conexão com o banco de dados
-const sequelize = new Sequelize('progdb', 'postgres', '1205', {
-    host: 'localhost',
-    port: '5432',
-    dialect: 'postgres',
-    define: {
-        timestamps: false,
-        freezeTableName: true
-    }
-});
-
-//Testando Conexão
-try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-
-//Mapeamento da Model Editora
-const Editora = sequelize.define(
-    'editora',
-    {
-      // Model attributes are defined here
-      ideditora: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      nomeeditora: {
-        type: DataTypes.STRING(60),
-        allowNull: false,
-      },
-      endereco: {
-        type: DataTypes.TEXT,
-        allowNull: true,
-      },
-      cnpj: {
-        type: DataTypes.STRING(20),
-        allowNull: false,
-      }
-    },
-  );
+import banco from "./banco.js";
+import editora from "./controller/EditoraController.js"
   
 const app = express();
 app.use(express.json());
@@ -55,61 +11,26 @@ app.get('/teste', (req, res)=>{
 })
 
 //Rotas Crud da tabela editoras
-app.get('/editora', async (req, res) => {
-    const respostaBanco = await Editora.findAll();
-    res.json(respostaBanco);
-});
+app.get('/editora', editora.listar)
 
-app.get('/editora/:id', async (req, res) => {
-    const id = req.params.id;
-    const respostaBanco = await Editora.findByPk(id);
-    res.json(respostaBanco);
-});
+app.get('/editora/:id', editora.selecionar);
 
-app.post('/editora', async (req, res) => {
-//    const nomeeditora = req.body.nomeeditora;
-//    const cnpj = req.body.cnpj;
-//    const endereco = req.body.endereco;
+app.post('/editora', editora.inserir);
 
-    const respostaBanco = await Editora.create(req.body);
-    res.json(respostaBanco);
-});
+app.put('/editora/:id', editora.alterar);
 
-app.put('/editora/:id', async (req, res) => {
-    const nomeeditora = req.body.nomeeditora;
-    const cnpj = req.body.cnpj;
-    const endereco = req.body.endereco;
-
-    const ideditora = req.params.id;
-    
-        const respostaBanco = await Editora.update(
-            {nomeeditora, cnpj, endereco},
-            {where: {ideditora}});
-        res.json(respostaBanco);
-    });
-
-app.put('/editora/:id', async (req, res) => {
-    const nomeeditora = req.body.nomeeditora;
-    const cnpj = req.body.cnpj;
-    const endereco = req.body.endereco;
-
-    const ideditora = req.params.id;
-    
-        const respostaBanco = await Editora.update(
-            {nomeeditora, cnpj, endereco},
-            {where: {ideditora}});
-        res.json(respostaBanco);
-    });
-
-app.delete('/editora/:id', async (req, res) => {
-    const ideditora = req.params.id;
-
-    const respostaBanco = await Editora.destroy(
-    {where: {ideditora}});
-    res.json(respostaBanco);
-});    
-
+app.delete('/editora/:id', editora.excluir);    
 
 app.listen(3000, ()=>{
-    console.log(`Servidor Rodando`)
+  console.log(`Servidor Rodando`)
 });
+
+//Testando Conexão
+try {
+  await banco.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+
+
